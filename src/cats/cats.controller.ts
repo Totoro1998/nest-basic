9eaf88c116d9ef2d5791080037c7d427bdeea2b6
totoro <totoro.fu@webeye.com>
2024-06-25 13:41:00 +0800

    代码优化

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
@@ -16,6 +26,12 @@ export class CatsController {
     console.log('originalUrl', req.originalUrl);
     return JSON.stringify({ name: 'fk' });
   }
+  // 如果需要混用响应方式，需要传{ passthrough: true }
+  @Get('test_res')
+  testRes(@Res({ passthrough: true }) res: Response): string {
+    res.setHeader('test_fk', 'fk');
+    return 'test_res';
+  }
   @Get(':id')
   findOne(@Param() params, @Query() query): string {
     // http://localhost:3100/cats/1?id=2
@@ -23,7 +39,6 @@ export class CatsController {
     console.log(query); //{ id: '2' }
     return params.id;
   }
-
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
